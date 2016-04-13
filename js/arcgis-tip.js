c7c95fd68e0bc17a76a72e2321a6da6623d04a79
Matt Yoder <myoder@ccrpc.org>
2016-04-13 08:53:11 -0500

    Initial work on column filtering.

--- js/arcgis-tip.js
+++ js/arcgis-tip.js
@@ -8,9 +8,7 @@ require([
   (function ($) {
     $(function () {
       $('.arcgis-tip').each(function () {
-        var columns =  ['ProjectID', 'FiscalYear', 'ProjectTitle', 'StartTerminus', 'EndTerminus', 'LeadAgency', 'AllAgencies', 'FundType', 'FederalFunding', 'StateFunding', 'LeadAgencyFunding', 'OtherAgencyFunding', 'TotalCost', 'Description', 'ProjectCategory', 'NonInfrastructure', 'MultiAgency', 'AdvancedConstruction'],
-          columnLabels = ['Project ID', 'Fiscal Year', 'Project Title', 'Start Terminus', 'End Terminus', 'Lead Agency', 'All Agencies', 'Fund Type', 'Federal Funding', 'State Funding', 'Lead Agency Funding', 'Other Agency Funding', 'Total Cost', 'Description', 'Project Category', 'Non Infrastructure', 'Multi Agency', 'Advanced Construction'],
-          mapServiceURL = $(this).data('service'),
+        var mapServiceURL = $(this).data('service'),
           layerQuery = "(FiscalYear >= 2017 AND FiscalYear <= 2020) OR AdvancedConstruction = 'Yes'",
           layerDefs = [layerQuery, layerQuery],
           pointTask = new QueryTask(mapServiceURL + '0'),
@@ -31,13 +29,108 @@ require([
             }
             return null;
           },
+          columns =  [
+            {
+              name: 'ProjectID',
+              title: 'Project ID'
+            },
+            {
+              name: 'FiscalYear',
+              title: 'Fiscal Year',
+              filter: true
+
+            },
+            {
+              name: 'ProjectTitle',
+              title: 'Project Title'
+            },
+            {
+              name: 'StartTerminus',
+              title: 'Start Terminus'
+            },
+            {
+              name: 'EndTerminus',
+              title: 'End Terminus'
+            },
+            {
+              name: 'LeadAgency',
+              title: 'Lead Agency',
+              filter: true
+            },
+            {
+              name: 'AllAgencies',
+              title: 'All Agencies',
+              filter: true
+            },
+            {
+              name: 'FundType',
+              title: 'Fund Type'
+            },
+            {
+              name: 'FederalFunding',
+              title: 'Federal Funding',
+              render: formatCurrency,
+              visible: false
+            },
+            {
+              name: 'StateFunding',
+              title: 'State Funding',
+              render: formatCurrency,
+              visible: false
+            },
+            {
+              name: 'LeadAgencyFunding',
+              title: 'Lead Agency Funding',
+              render: formatCurrency,
+              visible: false
+            },
+            {
+              name: 'OtherAgencyFunding',
+              title: 'Other Agency Funding',
+              render: formatCurrency,
+              visible: false
+            },
+            {
+              name: 'TotalCost',
+              title: 'Total Cost',
+              render: formatCurrency
+            },
+            {
+              name: 'Description',
+              title: 'Description'
+            },
+            {
+              name: 'ProjectCategory',
+              title: 'Project Category',
+              visible: false,
+              filter: true
+            },
+            {
+              name: 'NonInfrastructure',
+              title: 'Non-Infrastructure',
+              visible: false,
+              filter: true
+            },
+            {
+              name: 'MultiAgency',
+              title: 'Multi-Agency',
+              visible: false,
+              filter: true
+            },
+            {
+              name: 'AdvancedConstruction',
+              title: 'Advanced Construction',
+              visible: false,
+              filter: true
+            }
+          ],
           displayResults = function () {
             var rows = [];
             $.each(featureSets, function (i, featureSet) {
               $.each(featureSet.features, function (i, feature) {
                 var row = [];
                 $.each(columns, function (i, column) {
-                  row.push(feature.attributes[column]);
+                  row.push(feature.attributes[column.name]);
                 });
                 rows.push(row);
               });
@@ -45,22 +138,44 @@ require([
             $('#tip-table').DataTable({
               dom: 'Bfrtip',
               data: rows,
-              columns: $.map(columnLabels, function(label) {
-                return {title: label};
-              }),
-              columnDefs: [
-                {
-                  targets: [8, 9, 10, 11, 12],
-                  render: formatCurrency
-                },
-                {
-                  targets: [8, 9, 10, 11, 14, 15, 16, 17],
-                  visible: false
-                }
-              ],
+              paging: false,
+              info: false,
+              columns: columns,
               buttons: [
                 'colvis'
-              ]
+              ],
+              initComplete: function () {
+                var api = this.api(),
+                  table = api.table().node();
+
+                console.log(table);
+                $.each(columns, function(i, colDef) {
+                  if (colDef.filter) {
+                    console.log(colDef.name);
+                    var column = api.column(colDef.name + ':name'),
+                      wrapper = $('<div class="tip-filter"></div>')
+                        .insertBefore(table),
+                      label = $('<label></label>')
+                        .attr('for', colDef.name)
+                        .text(colDef.title + ':')
+                        .appendTo(wrapper),
+                      select = $('<select></select>')
+                        .attr('id', colDef.name)
+                        .attr('name', colDef.name)
+                        .append('<option value=""></option>')
+                        .appendTo(wrapper)
+                        .on('change', function () {
+                            var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                            column.search( val ? '^'+val+'$' : '', true, false ).draw();
+                        });
+
+                  column.data().unique().sort().each( function (d, j) {
+                    select.append('<option value="'+d+'">'+d+'</option>')
+                  });
+                  }
+                });
+
+              }
             });
           },
           success = function (featureSet) {
